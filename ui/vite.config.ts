590e5d8096869e6fe08205fadab5d151885b903d
Julian Schambeck <julian.schambeck@sap.com>
2024-03-20 13:43:55 +0100

    copy over template

---
+++ ui/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    hmr: {
+      port: 5174,
+    },
+  },
+  build: {
+    outDir: "../router/dist",
+    emptyOutDir: true,
+  },
+});
